d5647c6ed863e0a73b9f7efd6a0492ca7f9fecfa
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-08 13:55:39 +0100

    Explicitly trigger the attrs default for transform_mode

--- tests/hypothesis_strategies/targets.py
+++ tests/hypothesis_strategies/targets.py
@@ -1,6 +1,7 @@
 """Hypothesis strategies for targets."""
 
 import hypothesis.strategies as st
+from attrs import NOTHING
 
 from baybe.targets.enum import TargetMode
 from baybe.targets.numerical import _VALID_TRANSFORM_MODES, NumericalTarget
@@ -19,7 +20,11 @@ def numerical_target(draw: st.DrawFn):
     bounds = draw(
         interval(exclude_half_open=True, exclude_open=mode is TargetMode.MATCH)
     )
-    transform_mode = draw(st.sampled_from(_VALID_TRANSFORM_MODES[mode]))
+    transform_mode = draw(st.none() | st.sampled_from(_VALID_TRANSFORM_MODES[mode]))
+
+    # Explicitly trigger the attrs default method
+    transform_mode = transform_mode if transform_mode is not None else NOTHING
+
     return NumericalTarget(
         name=name, mode=mode, bounds=bounds, transform_mode=transform_mode
     )
