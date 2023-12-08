228734d76e5226d2e162ff0c8b65e71a93b2a678
AdrianSosic <adrian.sosic@merckgroup.com>
2023-12-08 11:32:20 +0100

    Add strategies for targets

---
+++ tests/hypothesis_strategies/targets.py
@@ -0,0 +1,29 @@
+"""Hypothesis strategies for targets."""
+
+import hypothesis.strategies as st
+
+from baybe.targets.enum import TargetMode
+from baybe.targets.numerical import _VALID_TRANSFORM_MODES, NumericalTarget
+
+from .utils import interval
+
+target_name = st.text(min_size=1)
+"""A strategy that generates target names."""
+
+
+@st.composite
+def numerical_target(draw: st.DrawFn):
+    """Generate class:`baybe.targets.numerical.NumericalTarget`."""
+    name = draw(target_name)
+    mode = draw(st.sampled_from(TargetMode))
+    bounds = draw(
+        interval(exclude_half_open=True, exclude_open=mode is TargetMode.MATCH)
+    )
+    transform_mode = draw(st.sampled_from(_VALID_TRANSFORM_MODES[mode]))
+    return NumericalTarget(
+        name=name, mode=mode, bounds=bounds, transform_mode=transform_mode
+    )
+
+
+target = numerical_target()
+"""A strategy that generates targets."""
